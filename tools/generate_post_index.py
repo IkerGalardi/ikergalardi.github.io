4fe9bdfebcb8c92c38b2e2f337ac4539d0a284fa
IkerGalardi <contacto.ikergalardi@gmail.com>
2026-01-30 17:28:10 +0100

    tools: metadata extraction tool
    For now only prints stuff

---
+++ tools/generate_post_index.py
@@ -0,0 +1,73 @@
+import os
+from datetime import date
+
+
+class PostMetadata:
+    def __init__(self, publication: date, title: str, first_paragraph: str):
+        self.publication = publication
+        self.title = title
+        self.first_paragraph = first_paragraph
+
+    def __lt__(self, other):
+        return self.publication < other.publication
+
+
+def print_metadata(metadata: PostMetadata) -> None:
+    print(f"Post '{metadata.title}' published on {metadata.publication}'")
+
+
+def extract_metadata_line(line: str) -> tuple[str, str]:
+    parts = line.strip().split(":")
+    return (parts[0], parts[1].strip())
+
+
+def extract_metadata(file: str) -> PostMetadata:
+    PRE_TRIPLE_SLASH = 0
+    META = 2
+    POST = 4
+
+    title: str | None = None
+    publication: date | None = None
+    first_paragraph: str | None = None
+
+    state = PRE_TRIPLE_SLASH
+    lines = open(file, "r").readlines()
+    for line in lines:
+        line = line[:-1]
+        if state == PRE_TRIPLE_SLASH:
+            if line == "---":
+                state = META
+        elif state == META:
+            if line == "---":
+                state = POST
+            else:
+                type, value = extract_metadata_line(line)
+                if type == "title":
+                    title = value
+                elif type == "date":
+                    date_splited = value.split("-")
+                    publication = date(
+                        int(date_splited[0]), int(date_splited[1]), int(date_splited[2])
+                    )
+        elif state == POST:
+            if len(line) > 0 and line[0] != "#":
+                first_paragraph = line.strip()
+                break
+
+    assert title is not None
+    assert publication is not None
+    assert first_paragraph is not None
+
+    return PostMetadata(publication, title, first_paragraph)
+
+
+posts_metadata = []
+directory = os.fsencode("posts")
+for filename in os.listdir(directory):
+    metadata = extract_metadata("posts/" + filename.decode())
+    posts_metadata.append(metadata)
+
+posts_metadata.sort(reverse=True)
+
+for metadata in posts_metadata:
+    print_metadata(metadata)
